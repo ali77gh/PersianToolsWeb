2a8810ae7aab0d2fd8ddfab2a8ec6314a46f0688
Ali Ghahremani <alighahremani1377@gmail.com>
2024-04-17 13:45:31 +0330

    feature: chips

--- src/ui/tool_list_item.rs
+++ src/ui/tool_list_item.rs
@@ -11,12 +11,13 @@ pub struct ToolWrapper {
 pub fn ToolListItem(props: ToolWrapper) -> Element {
     let tool = props.tool;
 
-    let tags = tool
-        .tags
-        .iter()
-        .map(|x| format!("#{}", x))
-        .collect::<Vec<String>>()
-        .join(", ");
+    let tags = tool.tags.iter().map(|tag| {
+        rsx! {
+            div { class: "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-1",
+                "#{tag}"
+            }
+        }
+    });
 
     rsx! {
         div {
@@ -24,7 +25,7 @@ pub fn ToolListItem(props: ToolWrapper) -> Element {
             class: "max-w-2xl rounded  shadow-lg bg-white m-5 p-5",
             div { class: "font-bold text-xl mb-2", "{tool.name}" }
             div { class: "text-gray-700 text-base mb-3", "{tool.description}" }
-            div { "{tags}" }
+            div { {tags} }
         }
     }
 }
